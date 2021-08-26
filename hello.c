4d506790fe132f318f31ab4e9fdbccdf0731d471
Brooke James <bjames16@huskers.unl.edu>
2021-08-26 22:10:18 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author:Brooke James
+ * Date: 8-24-21
+ * 
+ * A simple Hello world program
+ * 
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    
+    printf("Brooke James!\n");
+    
+    return 0;
+}
