ba2a1c523802b400337e9fe12452718960793ad3
PawEhThaYou <pthayou2@huskers.unl.edu>
2021-08-27 21:31:36 +0000

    I added my code

--- hello.c
+++ hello.c
@@ -12,6 +12,8 @@ int main(int argc, char **argv) {
     
     printf("Brooke James!:l\n");
     printf("Undecided\n");
+    printf("PawEhThaYou\n");
+    printf("Computer Science\n");
     
     return 0;
 }
